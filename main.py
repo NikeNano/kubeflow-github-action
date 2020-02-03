4d8bf98a1bcb2fe94b4dee2a39f8b7b1dcbbf0a2
NikeNano <niklas.hansson@sandvik.com>
2020-02-03 16:37:07 +0100

    updated the main file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import kfp
 import kfp.compiler as compiler
 import click
 import importlib.util
+from datetime import datetime
 
 
 def load_function(pipeline_function_name  :str, full_path_to_pipeline :str) -> object:
@@ -79,17 +80,56 @@ def find_pipeline_id(pipeline_name: str, client: kfp.Client, page_size: str, pag
             break
 
 
-def run_pipeline(pipeline_id: str, experiment_id: str, pipeline_paramters: dict):
+
+def find_experiment_id(experiment_name: str, client: kfp.Client) -> str:
+    """Function to return the experiment id
+    
+    Arguments:
+        experiment_name {str} -- The experiment name
+        client {kfp.Client} -- The kfp client
+    
+    Returns:
+        str -- The experiment id
+    """
+    while True: 
+        experiments = client.list_experiments(page_size=page_size, page_token=page_token)
+        for experiments in experiments.experiments: 
+            if experiments.name == experiments_name: 
+                return experiments.id
+        # Start need to know where to do next itteration from 
+        page_token = experiments.next_page_token
+        # If no next tooken break
+        if not page_token:
+            break
+
+
+def read_pipeline_params(pipeline_paramters_path:str ) -> dict: 
+    "Continue here"
+    #[TODO]
+    #https://stackoverflow.com/questions/13019653/converting-yaml-file-to-python-dict
+
+
+def run_pipeline(pipeline_name:str , pipeline_id: str, experiment_id: str, pipeline_paramters_path: dict):
+    experiment_id = find_experiment_id(experiment_name=os.environ["INPUT_EXPERIMENT_NAME"], 
+    client=client)
+    logging.info(f"The expriment id is: {experiment_id}")
+    if not experiment_id: 
+        raise ValueError(f"Failed to find experiment with the name: {os.environ["INPUT_EXPERIMENT_NAME"]}")
     if os.getenv["INPUT_PIPELINE_NAMESPACE"]:
         namespace = os.environ["INPUT_PIPELINE_NAMESPACE"]
+        logging.info(f"The namespace that will be used is: {namespace}")
+    #[TODO] What would be a good way to name the jobs
+    job_name = pipeline_name + datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    logging.info(f"The job name is: {job_name}")
+    pipeline_params = read_pipeline_params(pipeline_paramters_path=pipeline_paramters_path)
     client.run_pipeline(
         experiment_id=experiment_id, 
         job_name=job_name, 
-        params={}, # Read this as a yaml, people seam to prefer that to json.  
+        params=pipeline_params, # Read this as a yaml, people seam to prefer that to json.  
         pipeline_id=pipeline_id, 
         namespace=None))
-    
 
+    
 
 def main():
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.environ["INPUT_GOOGLE_APPLICATION_CREDENTIALS"]
@@ -105,8 +145,12 @@ def main():
     if os.getenv("run"):
         pipeline_id = find_pipeline_id(pipeline_name=pipeline_name,
             client=client)
-        experiment_id = find_experiment_id()
-        run_pipeline(pipeline_id=pipeline_id, client=client)
+        logging.info(f"The pipeline id is: {pipeline_id}")
+        run_pipeline(pipeline_name=pipeline_name, 
+            pipeline_id=pipeline_id, 
+            client=client, 
+            pipeline_paramters_path=os.environ["INPUT_PIPELINE_PARAMETERS_PATH"])
+
 
 if __name__ == "__main__": 
     main()
