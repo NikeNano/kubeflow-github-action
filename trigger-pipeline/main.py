159f26b39ab4429853c0e532d3751c3ce3614bf4
Diego Kiner <diegok@unity3d.com>
2022-02-09 00:29:27 +0000

    Add client

--- trigger-pipeline/main.py
+++ trigger-pipeline/main.py
@@ -180,6 +180,10 @@ def main():
     logging.info("Started the process to run the pipeline on kubeflow.")
     pipeline_id = os.getenv("INPUT_PIPELINE_ID")
     run_name = os.getenv("INPUT_RUN_NAME")
+    client = kfp.Client(
+        host=kubeflow_url,
+        client_id=client_id,
+    )
     run_pipeline(pipeline_name=run_name,
                  pipeline_id=pipeline_id,
                  client=client,
